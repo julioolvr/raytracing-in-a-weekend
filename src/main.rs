55656a75c0236e0daa9c1cc69badfaa374bfd07c
Julio <julio.olvr@gmail.com>
2019-09-15 19:00:09 -0300

    Output sample Wikipedia PPM image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use std::path::Path;
+use std::fs::File;
+use std::io::prelude::*;
+
+fn main() -> Result<(), std::io::Error> {
+    // Output PPM image
+    let path = Path::new("out/hello_world.ppm");
+    let mut file = File::create(path)?;
+
+    file.write(b"P3\n")?;
+    file.write(b"3 2\n")?;
+    file.write(b"255\n")?;
+
+    file.write(b"255   0   0     0 255   0     0   0 255\n")?;
+    file.write(b"255 255   0   255 255 255     0   0   0\n")?;
+
+    Ok(())
 }
