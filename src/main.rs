3e3664f7565cc4ab961d736263c69a9b06b6c95e
Julio <julio.olvr@gmail.com>
2019-09-16 00:13:36 -0300

    Vector3 struct draft

--- src/main.rs
+++ src/main.rs
@@ -14,17 +14,31 @@ fn main() -> Result<(), std::io::Error> {
 
     for x in (0..height).rev() {
         for y in 0..width {
-            let r = y as f64 / width as f64;
-            let g = x as f64 / height as f64;
-            let b = 0.2;
+            let color = Vector3::new(
+                y as f64 / width as f64,
+                x as f64 / height as f64,
+                0.2
+            );
 
-            let ir = (255.0 * r) as u8;
-            let ig = (255.0 * g) as u8;
-            let ib = (255.0 * b) as u8;
+            let ir = (255.0 * color.x) as u8;
+            let ig = (255.0 * color.y) as u8;
+            let ib = (255.0 * color.z) as u8;
 
             write!(file, "{} {} {}\n", ir, ig, ib)?;
         }
     }
 
     Ok(())
+}
+
+struct Vector3 {
+    x: f64,
+    y: f64,
+    z: f64,
+}
+
+impl Vector3 {
+    fn new(x: f64, y: f64, z: f64) -> Vector3 {
+        Vector3 { x, y, z }
+    }
 }
