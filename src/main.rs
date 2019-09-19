a9d9c3b557c574f18ebb2d09cb336dd1e31f9a97
Julio <julio.olvr@gmail.com>
2019-09-18 21:16:29 -0300

    Render a background gradient

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
-use std::path::Path;
 use std::fs::File;
 use std::io::prelude::*;
+use std::path::Path;
 
 fn main() -> Result<(), std::io::Error> {
-    // Output PPM image
+    write_hello_world()?;
+    write_background()?;
+    Ok(())
+}
+
+fn write_hello_world() -> Result<(), std::io::Error> {
     let path = Path::new("out/hello_world.ppm");
     let mut file = File::create(path)?;
 
@@ -17,7 +22,7 @@ fn main() -> Result<(), std::io::Error> {
             let color = Vector3::new(
                 f64::from(y) / f64::from(width),
                 f64::from(x) / f64::from(height),
-                0.2
+                0.2,
             );
 
             let ir = (255.0 * color.x) as u8;
@@ -31,6 +36,50 @@ fn main() -> Result<(), std::io::Error> {
     Ok(())
 }
 
+fn write_background() -> Result<(), std::io::Error> {
+    let path = Path::new("out/background.ppm");
+    let mut file = File::create(path)?;
+
+    let width = 200;
+    let height = 100;
+
+    writeln!(file, "P3\n{} {}\n255", width, height)?;
+
+    let lower_left_corner = Vector3::new(-2.0, -1.0, -1.0);
+    let horizontal = Vector3::new(4.0, 0.0, 0.0);
+    let vertical = Vector3::new(0.0, 2.0, 0.0);
+    let origin = Vector3::new(0.0, 0.0, 0.0);
+
+    for x in (0..height).rev() {
+        for y in 0..width {
+            let u = f64::from(y) / f64::from(width);
+            let v = f64::from(x) / f64::from(height);
+
+            let ray = Ray::new(
+                origin,
+                lower_left_corner + horizontal.scale(u) + vertical.scale(v)
+            );
+
+            let color = color_for(ray);
+
+            let ir = (255.0 * color.x) as u8;
+            let ig = (255.0 * color.y) as u8;
+            let ib = (255.0 * color.z) as u8;
+
+            writeln!(file, "{} {} {}", ir, ig, ib)?;
+        }
+    }
+
+    Ok(())
+}
+
+fn color_for(ray: Ray) -> Vector3 {
+    let unit_direction = ray.direction.unit();
+    let t = 0.5 * (unit_direction.y + 1.0);
+    Vector3::new(1.0, 1.0, 1.0).scale(1.0 - t) + Vector3::new(0.5, 0.7, 1.0).scale(t)
+}
+
+#[derive(Copy, Clone)]
 struct Vector3 {
     x: f64,
     y: f64,
@@ -40,5 +89,44 @@ struct Vector3 {
 impl Vector3 {
     fn new(x: f64, y: f64, z: f64) -> Vector3 {
         Vector3 { x, y, z }
+    }
+
+    fn scale(&self, factor: f64) -> Vector3 {
+        Vector3::new(self.x * factor, self.y * factor, self.z * factor)
+    }
+
+    fn magnitude(&self) -> f64 {
+        (self.x.powi(2) + self.y.powi(2) + self.z.powi(2)).sqrt()
+    }
+
+    fn unit(&self) -> Vector3 {
+        self.scale(1.0 / self.magnitude())
+    }
+}
+
+impl std::ops::Add<Vector3> for Vector3 {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self {
+        Vector3::new(self.x + rhs.x, self.y + rhs.y, self.z + rhs.z)
+    }
+}
+
+impl std::ops::Mul<f64> for Vector3 {
+    type Output = Self;
+
+    fn mul(self, rhs: f64) -> Self {
+        Vector3::new(self.x * rhs, self.y * rhs, self.z * rhs)
+    }
+}
+
+struct Ray {
+    origin: Vector3,
+    direction: Vector3,
+}
+
+impl Ray {
+    fn new(origin: Vector3, direction: Vector3) -> Ray {
+        Ray { origin, direction }
     }
 }
